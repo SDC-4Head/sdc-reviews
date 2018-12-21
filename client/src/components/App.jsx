d545b6c8612ebc43365270537cf70c3106b4c8ef
James Tsukamoto <jaytsuk@gmail.com>
2018-12-20 20:01:52 -0800

    Complete initial skeleton build-out of service
    In this commmit, I've hard-coded the basic structure and layout
    of the Reviews service. In my next commit, I'll be working on connecting
    the database to the client to allow it to pull data off to populate the
    front end instead of using mock data.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,41 @@
+/* eslint-disable import/extensions */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-unused-state */
 // This component is the highest-level app that is responsible for assembling
 // each individual component. This is what is ultimately rendered onto the HTML page.
 import React from 'react';
+import ReviewSummary from './ReviewSummary.jsx';
+import ReviewBreakdown from './ReviewBreakdown.jsx';
+import ReviewList from './ReviewList.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      overallRating: 0,
+      numberOfReviews: 0,
+      reviewBreakdown: {
+        accuracy: 5,
+        communication: 5,
+        cleanliness: 5,
+        location: 5,
+        checkin: 5,
+        value: 5
+      }
+    };
   }
 
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <ReviewSummary
+          overall={this.state.overallRating}
+          qtyOfReviews={this.state.numberOfReviews}
+        />
+        <ReviewBreakdown breakdown={this.state.reviewBreakdown} />
+        <ReviewList />
+      </div>
+    );
   }
 }
 
--- client/src/components/ReviewItem.jsx
+++ client/src/components/ReviewItem.jsx
@@ -1,2 +1,22 @@
 // This component is responsible for displaying each individual review including the body,
 // the user's name, the user's photo, the date they stayed, and the "flag" option.
+import React from 'react';
+
+const ReviewItem = () => (
+  <div>
+    <img src="http://lorempixel.com/150/150" alt="user avatar" />
+    <span>Joleen</span>
+    <span>October 2018</span>
+    <div>
+      <p>
+        Perfect place in the perfect location! We are young parents who just needed time to
+        ourselves. This was the perfect location! We were in North Park, where there delicious food,
+        places to explore, close to walk around North Park, but also close uber/lyft rides or
+        scooter rides to downtown Gaslamp, Little Italy, and much more! Love that its two stories in
+        a great neighborhood! HIGHLY RECOMMEND!
+      </p>
+    </div>
+  </div>
+);
+
+export default ReviewItem;
