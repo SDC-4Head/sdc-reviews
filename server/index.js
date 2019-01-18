587a0db45bc04ec820947d71c8b00a66c767d40a
Francis Distor <francis.distor@gmail.com>
2019-01-18 11:32:01 -0800

    Set up redis. Added redis to get route.

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('../db/pool.js');
+const redis = require('redis');
 
 const app = express();
 const port = 3124;
+
+const client = redis.createClient();
+
+client.on('connect', () => console.log('Connected to Redis.'));
+client.on('error', err => console.log('Failed to connect to Redis.'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/rooms/:roomid', express.static('./public/dist'));
@@ -15,17 +21,26 @@ app.get('/api/reviews/rooms/:roomid/', (req, res) => {
   const { search } = req.query;
   const { sortby } = req.query || 'relevant';
 
-  pg
-    .getReviews(roomid)
-    .then(reviews => 
-      sortby === 'relevant' || sortby === undefined
-        ? pg.sortByRelevant(reviews)
-        : pg.sortByRecent(reviews)
-    )
-    .then(sortedReviews => pg.getBySearchTerm(sortedReviews, search))
-    .then(sortedReviews => pg.getPage(page, sortedReviews))
-    .then(pageOfReviews => res.send(pageOfReviews))
-    .catch(err => { if (err) throw err; });
+  client.get(`/reviews/${roomid}`, (err, result) => {
+    if (err || result === null) {
+      return pg
+      .getReviews(roomid)
+      .then(reviews => 
+        sortby === 'relevant' || sortby === undefined
+          ? pg.sortByRelevant(reviews)
+          : pg.sortByRecent(reviews)
+      )
+      .then(sortedReviews => pg.getBySearchTerm(sortedReviews, search))
+      .then(sortedReviews => pg.getPage(page, sortedReviews))
+      .then(pageOfReviews => {
+        client.set(`/reviews/${roomid}`, JSON.stringify(pageOfReviews), redis.print);
+        res.send(pageOfReviews);
+      })
+      .catch(err => { if (err) throw err; });
+    }
+    res.send(JSON.parse(result));
+  });
+
 });
 
 app.get('/api/ratings/rooms/:roomid', (req, res) => {
