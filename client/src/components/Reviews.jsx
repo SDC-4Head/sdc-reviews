846e6d9987464039f5c9094db7b977faf883ee88
Francis Distor <francis.distor@gmail.com>
2019-01-19 11:17:37 -0800

    Moved ratings functions to ReviewList.

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -24,14 +24,59 @@ class Reviews extends React.Component {
     this.getRatings(window.location.pathname + window.location.search);
   }
 
+  calculateAverageRating (allReviews) {
+    const ratings = {
+      quantity: allReviews.length,
+      overall: 0,
+      accuracy: 0,
+      communication: 0,
+      cleanliness: 0,
+      location: 0,
+      checkin: 0,
+      value: 0
+    };
+
+    allReviews.forEach(review => {
+      ratings.accuracy = (ratings.accuracy + review.accuracy) / 2;
+      ratings.communication = (ratings.communication + review.communication) / 2;
+      ratings.cleanliness = (ratings.cleanliness + review.cleanliness) / 2;
+      ratings.location = (ratings.location + review.location) / 2;
+      ratings.checkin = (ratings.checkin + review.checkin) / 2;
+      ratings.value = (ratings.value + review.value) / 2;
+    });
+
+    ratings.overall =
+      (ratings.accuracy +
+        ratings.communication +
+        ratings.cleanliness +
+        ratings.location +
+        ratings.checkin +
+        ratings.value) /
+      6;
+
+    const roundToHalfValue = () => {
+      ratings.accuracy = Math.round(ratings.accuracy * 2) / 2;
+      ratings.communication = Math.round(ratings.communication * 2) / 2;
+      ratings.cleanliness = Math.round(ratings.cleanliness * 2) / 2;
+      ratings.location = Math.round(ratings.location * 2) / 2;
+      ratings.checkin = Math.round(ratings.checkin * 2) / 2;
+      ratings.value = Math.round(ratings.value * 2) / 2;
+      ratings.overall = Math.round(ratings.overall * 2) / 2;
+    };
+
+    roundToHalfValue();
+    return ratings;
+  }
+
   getRatings(path) {
     $.ajax({
       url: `/api/ratings${path}`,
       type: 'GET',
       dataType: 'json',
       success: data => {
+        const averageRatings = this.calculateAverageRating(data);
         this.setState({
-          ratings: data
+          ratings: averageRatings
         });
       },
       error: err => {
