dac68e64590e5425b04c7990bbe7a002b1e02810
Francis Distor <francis.distor@gmail.com>
2019-01-23 13:44:58 -0800

    Disabled redis.

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ const port = 3124;
 
 const client = redis.createClient();
 
-client.on('connect', () => console.log('Connected to Redis.'));
-client.on('error', err => console.log('Failed to connect to Redis.'));
-client.flushall((err, success) => {
-  if (err) { return console.log('Could not flush.'); }
-  console.log('Success in flush!');
-});
+// client.on('connect', () => console.log('Connected to Redis.'));
+// client.on('error', err => console.log('Failed to connect to Redis.'));
+// client.flushall((err, success) => {
+//   if (err) { return console.log('Could not flush.'); }
+//   console.log('Success in flush!');
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/rooms/:roomid', express.static('./public/dist'));
@@ -27,21 +27,23 @@ app.get('/api/reviews/rooms/:roomid/', (req, res) => {
   const { search } = req.query;
   const { sortby } = req.query || 'relevant';
 
-  client.get(`reviews/${roomid}`, (err, result) => {
-    if (err || result === null) {
-      return pg
+  // client.get(`reviews/${roomid}`, (err, result) => {
+  //   if (err || result === null) {
+      // return 
+      pg
       .getReviews(roomid)
       .then(reviews => {
-        client.set(`reviews/${roomid}`, JSON.stringify(reviews), 'EX', 600);
+        // client.set(`reviews/${roomid}`, JSON.stringify(reviews), 'EX', 600);
         res.send(reviews);
         }
       )
       .catch(err => { if (err) throw err; });
     }
-    res.send(JSON.parse(result));
-  });
+    // res.send(JSON.parse(result));
+  // });
 
-});
+// }
+);
 
 app.get('/api/ratings/rooms/:roomid', (req, res) => {
   const { roomid } = req.params;
