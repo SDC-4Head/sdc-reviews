de5793d3655df633e46c10ede37e5416f4126403
Francis Distor <francis.distor@gmail.com>
2019-01-24 12:11:36 -0800

    Uncommented redis.

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('../db/pool.js');
 const path = require('path');
-// const redis = require('redis');
+const redis = require('redis');
 // const morgan = require('morgan');
 
 const app = express();
 const port = 3124;
 
-// const client = redis.createClient();
+const client = redis.createClient();
 
-// client.on('connect', () => console.log('Connected to Redis.'));
-// client.on('error', err => console.log('Failed to connect to Redis.'));
-// client.flushall((err, success) => {
-//   if (err) { return console.log('Could not flush.'); }
-//   console.log('Success in flush!');
-// });
+client.on('connect', () => console.log('Connected to Redis.'));
+client.on('error', err => console.log('Failed to connect to Redis.'));
+client.flushall((err, success) => {
+  if (err) { return console.log('Could not flush.'); }
+  console.log('Success in flush!');
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/rooms/:roomid', express.static('./public/dist'));
@@ -28,41 +28,36 @@ app.get('/api/reviews/rooms/:roomid/', (req, res) => {
   const { search } = req.query;
   const { sortby } = req.query || 'relevant';
 
-  // client.get(`reviews/${roomid}`, (err, result) => {
-  //   if (err || result === null) {
-      // return 
-      pg
+  client.get(`reviews/${roomid}`, (err, result) => {
+    if (err || result === null) {
+      return pg
       .getReviews(roomid)
       .then(reviews => {
-        // client.set(`reviews/${roomid}`, JSON.stringify(reviews), 'EX', 600);
+        client.set(`reviews/${roomid}`, JSON.stringify(reviews), 'EX', 600);
         res.send(reviews);
         }
       )
       .catch(err => { if (err) throw err; });
     }
-    // res.send(JSON.parse(result));
-  // });
-
-// }
-);
+    res.send(JSON.parse(result));
+  });
+});
 
 app.get('/api/ratings/rooms/:roomid', (req, res) => {
   const { roomid } = req.params;
-  // client.get(`ratings/${roomid}`, (err, result) => {
-    // if (err || result === null) {
-      // return 
-      pg
+  client.get(`ratings/${roomid}`, (err, result) => {
+    if (err || result === null) {
+      return pg
         .getReviews(roomid)
         .then(reviews => {
-          // client.set(`ratings/${roomid}`, JSON.stringify(reviews), 'EX', 600);
+          client.set(`ratings/${roomid}`, JSON.stringify(reviews), 'EX', 600);
           res.send(reviews);
         })
         .catch(err => { if (err) throw err; });
     }
-    // res.send(JSON.parse(result));
-  // });
-// }
-);
+    res.send(JSON.parse(result));
+  });
+});
 
 app.get('/loaderio-cede38c6d273ec58a64293c11798e730', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/dist', 'loaderio-cede38c6d273ec58a64293c11798e730.txt'))
